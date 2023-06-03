3594bac1b39907d226b29049279f40a2a7ae9c07
conectaApi <laercio@conectaapi.com>
2023-06-02 21:08:16 -0300

    laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+class HomeController
+{
+    public function index()
+    {
+        echo '<h1>Bem vindo</h1>';
+    }
+    public function show($id)
+    {
+        echo "<h1>Bem vindo $id</h1>";
+    }
+}
